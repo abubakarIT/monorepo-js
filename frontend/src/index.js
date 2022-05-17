33a6a9a2f2b25081bb09f4436921256b5dc2fc87
Abubakar Mukhtar <abubakarmukhtar912@gmail.com>
2022-05-17 13:55:54 +0100

    Make some changes in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
 document.onload = function (e) {
    const root = document.getElementById('root');
-   root.innerText = Welcome to the Monorepo-JS Scaffold!
+   root.innerText = "Welcome to the Monorepo-JS Scaffold!";
 }
